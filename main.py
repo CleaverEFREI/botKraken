ce0df63f3fed7a5a09a5afe38c34891c7da81f67
Cleaver <louis.gailhac@efrei.net>
2022-01-19 18:29:37 +0100

    V2 Utilisation de l'API metamob

--- main.py
+++ main.py
@@ -8,45 +8,75 @@ from dotenv import load_dotenv
 from discord.utils import get
 from lxml import html
 
+load_dotenv(dotenv_path="config")
 
-bot = commands.Bot(command_prefix="!", description = "Bot Krala")
+bot = commands.Bot(command_prefix="!", description="Bot Krala")
+
+
 @bot.event
 async def on_ready():
-	print("Ready !")
+    print("Ready !")
 
 
 @bot.command()
-async def krala(ctx, serv_id, role_name):    
+async def krala(ctx, serv, role_name, alarmtime = 2, checktime=3600):
     while True:
         Krala = get(ctx.guild.roles, name=role_name)
-        serv = str(serv_id)
         now = datetime.datetime.now()
 
         headers = {
-            'authority': 'metamob.fr',
-            'referer': 'https://metamob.fr/kralamoure',
+            "HTTP-X-APIKEY": os.getenv("API"),
         }
 
         params = (
-            ('date', str(now.year).zfill(4)+'-'+str(now.month).zfill(2)+'-'+str(now.day).zfill(2)),
-            ('serveur', serv),
+            (
+                "date_debut",
+                str(now.year).zfill(4)
+                + "-"
+                + str(now.month).zfill(2)
+                + "-"
+                + str(now.day).zfill(2),
+            ),
+            (
+                "date_fin",
+                str(now.year).zfill(4)
+                + "-"
+                + str(now.month).zfill(2)
+                + "-"
+                + str(now.day + 1).zfill(2),
+            ),
+            ("serveur", serv),
         )
 
-        response = requests.get('https://metamob.fr/kralamoure', headers=headers, params=params)
+        response = requests.get(
+            "https://api.metamob.fr/kralamoures", headers=headers, params=params
+        )
 
-        webpage = html.fromstring(response.content)
-        list_of_url = webpage.xpath("//a/@href[contains(., 'evenement?id=')]")
+        for rep in response.json():
+            req_crea = requests.get(
+                "https://api.metamob.fr/utilisateurs/" + rep["createur"],
+                headers=headers,
+            )
+            d = rep["date"]
+            d = d.split("-")
+            d = d[:2] + d[2].split(" ")
+            if (
+                (d[2] == str(now.day).zfill(2))
+                and (d[1] == str(now.month).zfill(2))
+                and (d[0] == str(now.year).zfill(4))
+                and (now.hour+alarmtime>int(d[3].split(":")[0]))
+                and (now.hour<=int(d[3].split(":")[0]))
+            ):
+                await ctx.send(
+                    f"""{Krala.mention}
+                    :pushpin: Ouverture prévue bientôt ! :pushpin: 
+                    :alarm_clock:  {d[3]} 
+                    :pencil:  {rep['nombre_utilisateurs']} personnes inscrites
+                    :notepad_spiral:  {rep['description']}
+                    Pseudo créateur: {req_crea.json()['pseudo']}, Contact : {req_crea.json()['contact']}
+                    https://metamob.fr/kralamoure/evenement?id={rep['id']}"""
+                )
+        time.sleep(checktime)
 
-        for id_url in list_of_url:
-            time.sleep(2)
-            rep_event = requests.get('https://metamob.fr'+id_url, headers=headers, params=params)
-            d = rep_event.text.split('<table class="table table-bordered table-striped">\r\n\t\t\t<tbody>\r\n\t\t\t\t<tr>\r\n\t\t\t\t\t<td>\r\n\t\t\t\t\t\t<p>Date</p>\r\n\t\t\t\t\t</td>\r\n\t\t\t\t\t<td>\r\n\t\t\t\t\t\t<p>')[1].split("</p>")[0]
-            d = d.split("/")
-            if (d[0] == str(now.day).zfill(2)) and (d[1] == str(now.month).zfill(2)) and (d[2].split(" ")[0] == str(now.year).zfill(4)[2:]) and (d[2].split(" ")[1].split(":")[0] == str(now.hour).zfill(2) or d[2].split(" ")[1].split(":")[0] == str(now.hour+1).zfill(2)) :
-                time_open = d[2].split(" ")[1]
-                await ctx.send(f"{Krala.mention} Ouverture prevue aujourd'hui à {time_open}")
-        time.sleep(10)
 
-
-load_dotenv(dotenv_path="config")
 bot.run(os.getenv("TOKEN"))
